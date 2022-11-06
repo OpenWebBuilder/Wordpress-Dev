52e6fdf122bde2ad9ede6092ecde0e0e4fc590ef
DarkStar <darkstar@unisocial.net>
2022-11-06 11:39:27 -0600

    wp-cli example

---
+++ ansible/roles/WP-CLI/example/files/wp-cli.example.sh
@@ -0,0 +1,22 @@
+---
+
+url=example.com
+site_title="1PR Brands"
+password="adminpass"
+email="admin@1prbrands.local"
+
+wp core install --url=$url --title=$site_title --admin_password=$password --admin_email=$email --skip-email
+
+
+create_config(){
+wordpress_home=/var/www/wordpress/
+wordpress_db=wpdb
+wordpress_db_user=wpdbuser
+wordpress_db_pass=12345dd
+
+cd $wordpress
+wp config create --dbname=$wordpress_db --dbuser=$wordpress_db_user --dbpass=$wordpress_db_pass --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+PHP
+}
